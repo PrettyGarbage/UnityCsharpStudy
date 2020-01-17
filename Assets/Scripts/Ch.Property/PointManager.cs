c52bf6a1786b03ce64d10fa6a2e7f32667c39f44
Song <hiko1931@naver.com>
2020-01-17 10:09:47 +0900

    프로퍼티 추가

---
+++ Assets/Scripts/Ch.Property/PointManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointManager : MonoBehaviour
+{
+    private int m_point = 0;
+    public int point
+    {
+        get
+        {
+            Debug.Log(m_point);
+            return m_point;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                m_point = 0;
+            }
+            else if (value > 10000)
+            {
+                point = 10000;
+            }
+            else
+            {
+                m_point = value;
+
+            }
+        }
+    }
+
+    //SETGET을 사용하면 아래와 같이
+    //private int point = 0;
+
+    //public void SetPoint(int newPoint)
+    //{
+    //    if(newPoint < 0)
+    //    {
+    //        point = 0;
+    //    }
+    //    else if(newPoint > 10000)
+    //    {
+    //        point = 10000;
+    //    }
+    //    else
+    //    {
+    //        point = newPoint;
+    //    }
+    //}
+
+    //public int GetPoint()
+    //{
+    //    Debug.Log(point);
+    //    return this.point;
+    //}
+}
---
+++ Assets/Scripts/Ch.Property/PropertTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropertTest : MonoBehaviour
+{
+    public PointManager pointManager;
+
+    private void Start()
+    {
+        pointManager.point = 1000;
+        pointManager.point = -10000;
+
+        //Set Get을 이용한 방식
+
+        //pointManager.SetPoint(100);
+
+        //int myPoint = pointManager.GetPoint();
+        //Debug.Log("현재 포인트 : " + myPoint);
+
+        //pointManager.SetPoint(-10000);
+        //myPoint = pointManager.GetPoint();
+        //Debug.Log("현재 포인트 : " + myPoint);
+    }
+}
